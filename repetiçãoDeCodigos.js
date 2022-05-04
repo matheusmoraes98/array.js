b3d83a2964114922ba1e51df237f1812bc9ad40f
matheusmoraes98 <93088758+matheusmoraes98@users.noreply.github.com>
2022-05-04 04:47:43 -0300

    Add files via upload

---
+++ repetiçãoDeCodigos.js
@@ -0,0 +1,13 @@
+console.log(`trabalhando com listas`);
+
+const array =  [];
+
+array.push(`açucar`),
+array.push(`tempero`)
+array.push(`e tudo que a de bom`)
+array.push(`cafeteira`)
+
+
+console.log("preparoParaCafe");
+console.log(array[0]);
+console.log(array[2]);
